98a69e93b46e9f0e7bbece2ce7cc85689ba70466
Pruthvish <vasant.patel881997@gmail.com>
2024-02-15 16:35:22 +0530

    fix bug from internal transfer

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -6,7 +6,8 @@ class StockPicking(models.Model):
 
     def action_confirm(self):
         res = super(StockPicking, self).action_confirm()
-        for data in self:
-            for line in data.move_ids:
-                line.location_id = line.sale_line_id.delivery_location_id.id
+        if self.sale_id.order_line:
+            for data in self:
+                for line in data.move_ids:
+                    line.location_id = line.sale_line_id.delivery_location_id.id
         return res
